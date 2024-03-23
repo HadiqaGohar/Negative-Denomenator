377c36066fa200f6055d0e5e154089bf308bf7f0
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-23 14:04:58 -0700

    %

---
+++ main.py
@@ -0,0 +1,19 @@
+# .Remainder is negative when denomenator is  negative
+
+# Example 1 
+
+a,b = -5,2
+c = a%b
+print (c)           #1
+
+# Example 2
+
+a,b = 5,2
+c = a%b
+print (c)           #1
+
+# Example 3
+
+a,b = 5,-2
+c = a%b
+print (c)           #-1
